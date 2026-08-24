2af440ff100d2967af6e3528267f4bd8b1dfcc53
Noman-auditor <nomankhan2023bu@gmail.com>
2026-08-25 01:32:31 +0600

    Fix x402 module not found error via transpilePackages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    '@reown/appkit',
+    '@reown/appkit-adapter-wagmi',
+    '@wagmi/connectors',
+    '@base-org/account',
+    '@coinbase/cdp-sdk'
+  ],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' }
